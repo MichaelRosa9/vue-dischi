6a18955d85be77d0e5ce5029d59ffe7d4a78f42c
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-24 14:53:35 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+   <img src="../assets/img/spotify-logo.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/vars.scss';
+
+header {
+  height: 70px;
+  background-color: $color;
+  display: flex;
+  align-items: center;
+}
+
+img {
+  height: 80%;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <div class="container">
+      <Card />
+
+    </div>
+  </main>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+export default {
+  name: 'Main',
+  props: {
+
+  },
+  components: {
+    Card
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
