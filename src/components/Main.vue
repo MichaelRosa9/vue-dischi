67fae38d63c77230a441d691b49262672eb58b80
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-24 19:57:31 +0200

    Loader

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,31 +1,35 @@
 <template>
   <main>
-    <div class="container flex-center-between wrap gap-40">
+    <div v-if="!loading" class="container flex-center-between wrap gap-40">
       <Card class="basis-20-perc"
         v-for="(card, index) in cards" :key="index" 
         :card="card"
       />      
     </div>
+
+    <Loader />
   </main>
 </template>
 
 <script>
 
-import Card from '@/components/Card.vue';
 import axios from 'axios';
-
+import Card from '@/components/Card.vue';
+import Loader from '@/components/Loader.vue';
 export default {
   name: 'Main',
   data(){
     return {
       axios,
-      cards: []
+      cards: [],
+      loading: true
     }
   },
   created() {
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
     .then(result => {      
       this.cards = result.data.response;
+      this.loading = false;
       console.log(this.cards);
     })
     .catch(err => {
@@ -33,7 +37,8 @@ export default {
     });
   },
   components: {
-    Card
+    Card,
+    Loader
   }
 }
 </script>
